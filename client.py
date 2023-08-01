ccf8e6a143a54706b0e9e99056c656b89577a110
Abdarrahmane NEINE <abdarrahmane@MacBook-Pro-de-Abdarrahmane.local>
2023-08-02 00:35:13 +0200

    new version, with multiple processes

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import logging
+import re
 
 # server to listen from
 PORT = 5050
@@ -6,9 +8,37 @@ PORT = 5050
 SERVER = socket.gethostbyname("localhost")
 
 ADDRESS = (SERVER,PORT)
+logging.basicConfig(filename='result.txt',level=logging.INFO)
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
-    sock.connect(ADDRESS)
-    sock.sendall(b'Hello world')
-    data = sock.recv(1024)
-print("Received = ",repr(data))
+
+def is_valid_operation(operation):
+    # pattern: optional leading white spaces followed by an optional "-" (for negative numbers), 
+    # followed by one or more digits, then any number of groups that consist of: 
+    # white spaces, an operator (+, -, *, /), white spaces, optional "-" (for negative numbers), 
+    # and one or more digits, ending with optional trailing white spaces
+    pattern = r"^\s*-?\d+(?:\s*[-+*/]\s*-?\d+)*\s*$"
+    return bool(re.match(pattern, operation))
+try:
+    # data to send
+    # with open("op_test.txt", "r") as file:
+    #     operations = file.readlines()
+
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+        sock.connect(ADDRESS)
+        operations = []
+        with open("operations.txt", "r") as file:
+            operations = file.readlines()
+            
+        for i in range(0, len(operations), 3):  # iterate over operations in groups of 3
+            operation_batch = [op.rstrip('\n') for op in operations[i:i+3]]
+            operations_str = '|'.join(operation for operation in operation_batch if is_valid_operation(operation))
+            sock.sendall(operations_str.encode()) 
+            data = sock.recv(1024)
+            for operation, result in zip(operation_batch, data.decode().split('|')):
+                logging.info(operation + ' = ' + result)
+except FileNotFoundError:
+    logging.error("File not found.")
+except ConnectionError:
+    logging.error("Connection error.")
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
+import os
 import logging
-from multiprocessing import Process, Pipe
 
 # server to listen from
 PORT = 5050
@@ -9,15 +9,113 @@ SERVER = socket.gethostbyname("localhost")
 
 ADDRESS = (SERVER,PORT)
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
-    sock.bind(ADDRESS)
-    # listen only to 1 client
-    sock.listen(1)
-    connection, addr = sock.accept()
-    with connection:
-        print('connected by', addr)
-        while True:
-            data = connection.recv(1024)
-            if not data: break
-            print('data = ',data)
-            connection.sendall(data)
+logging.basicConfig(filename='server_log.txt',level=logging.INFO)
+
+MAX_PROCESSES = 4  # change to a suitable number for your system
+
+def calculateOperation(operation):
+    try:
+        operation = operation.split()
+        while "*" in operation or "/" in operation:
+            for i in range(len(operation)):
+                if operation[i] == "*":
+                    result = float(operation[i-1]) * float(operation[i+1])
+                    operation[i-1] = result
+                    del operation[i:i+2]
+                    break
+                elif operation[i] == "/":
+                    result = float(operation[i-1]) / float(operation[i+1])
+                    operation[i-1] = result
+                    del operation[i:i+2]
+                    break
+        while "+" in operation or "-" in operation:
+            for i in range(len(operation)):
+                if operation[i] == "+":
+                    result = float(operation[i-1]) + float(operation[i+1])
+                    operation[i-1] = result
+                    del operation[i:i+2]
+                    break
+                elif operation[i] == "-":
+                    result = float(operation[i-1]) - float(operation[i+1])
+                    operation[i-1] = result
+                    del operation[i:i+2]
+                    break
+        return result
+    except ValueError:
+        logging.error(f"Invalid operation: {operation}")
+        return 0
+    except ZeroDivisionError as z:
+        logging.error(z)
+        return 0
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+        return 0
+
+
+try:
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+        sock.bind(ADDRESS)
+        # listen only to 1 client
+        sock.listen(1)
+        connection, client_address = sock.accept()
+        with connection:
+            while True:
+                data = connection.recv(1024)
+                if not data: 
+                    break
+                # split the operations
+                operations = data.decode().split('|')
+
+                results = []  # this will hold the results from each process
+                child_processes = [None] * MAX_PROCESSES
+                child_pipes = [None] * MAX_PROCESSES
+
+                for operation in operations:
+                    # find an available child process
+                    for i in range(MAX_PROCESSES):
+                        if child_processes[i] is None or os.waitpid(child_processes[i], os.WNOHANG) != (0, 0):
+                            # this child process is available
+                            break
+                    else:
+                        # all child processes are busy, wait for one to finish
+                        pid, _ = os.wait()
+                        i = child_processes.index(pid)
+
+                    # if this child process was previously used, close its old pipe
+                    if child_pipes[i] is not None:
+                        os.close(child_pipes[i])
+
+                    # create a new pipe for this child process
+                    parent_conn, child_conn = os.pipe()
+                    child_pipes[i] = parent_conn
+
+                    new_process_id = os.fork()
+                    if new_process_id == 0:  # child process
+                        os.close(parent_conn)
+                        result = calculateOperation(operation)
+                        os.write(child_conn, str(result).encode())
+                        os.close(child_conn)
+                        os._exit(0)  # terminate the child process
+
+                    # parent process
+                    child_processes[i] = new_process_id
+                    os.close(child_conn)
+
+                # wait for all child processes to finish
+                for i in range(MAX_PROCESSES):
+                    if child_processes[i] is not None:
+                        os.waitpid(child_processes[i], 0)
+                        result = os.read(child_pipes[i], 1024).decode()  # added the decode call
+                        results.append(result)
+                        os.close(child_pipes[i])
+
+                # send all results back to the client
+                connection.sendall('|'.join(results).encode())
+            connection.close()
+except OSError as e:
+    if e.errno == 24:
+        logging.error("Too many open files.")
+    else:
+        logging.error(f"OS error: {e}")
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
